be708c32265c1ed97a3f00c8cf37968fbc730da4
LeandroBruno-1999 <62851869+LeandroBruno-1999@users.noreply.github.com>
2020-07-19 14:13:41 -0300

    Fundamentos
    Estudando Javascript

---
+++ organizacao.js
@@ -0,0 +1,8 @@
+console.log("Sentença de código") // Sentença de codigo
+
+{
+    {
+        console.log("Sentença de código")
+        console.log("Sentença de código")
+    } //Blocos de códigos
+}
